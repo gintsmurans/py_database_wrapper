6d74b4b332f9dc1ce010071ad1de037f0fa8331c
info@4apps.lv <Build Machine>
2024-11-07 23:41:45 +0000

    Added ignore_unknown_kwargs

--- src/db_wrapper/db_wrapper/config.py
+++ src/db_wrapper/db_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "78ee731595a915eacd68f7bd3b63cb0711437509",
+    "git_commit_hash": "8cd374e233dd4c1783385402747448207edeb749",
     "git_commit_date": "07.11.2024 23:27",
-    "app_version": "0.1.7",
+    "app_version": "0.1.8",
 }
--- src/db_wrapper/db_wrapper/utils/__init__.py
+++ src/db_wrapper/db_wrapper/utils/__init__.py
@@ -1,4 +1,9 @@
 from .return_model import ReturnModel
 from .timer import Timer
+from .dataclass_addons import ignore_unknown_kwargs
 
-__all__ = ["ReturnModel", "Timer"]
+__all__ = [
+    "ReturnModel",
+    "Timer",
+    "ignore_unknown_kwargs",
+]
