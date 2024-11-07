78ee731595a915eacd68f7bd3b63cb0711437509
info@4apps.lv <Build Machine>
2024-11-07 23:27:05 +0000

    Fixed commit info filename

--- src/db_wrapper/db_wrapper/config.py
+++ src/db_wrapper/db_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "79fc1a543e2f3d6e8426b2457402eecc900b14ef",
-    "git_commit_date": "07.11.2024 15:59",
-    "app_version": "0.1.5",
+    "git_commit_hash": "0ff372e90aca9e68c20468a79ebf92e067c40512",
+    "git_commit_date": "07.11.2024 23:26",
+    "app_version": "0.1.6",
 }
