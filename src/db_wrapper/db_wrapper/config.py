070d2a49a8dbfdcbb3722d499458d8412a40f2e0
info@4apps.lv <Build Machine>
2024-11-08 12:32:28 +0000

    Added github workflow

--- src/db_wrapper/db_wrapper/config.py
+++ src/db_wrapper/db_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "5b28ad0931341ff010c447d8845dd8ad7c527d0e",
-    "git_commit_date": "08.11.2024 12:00",
-    "app_version": "0.1.19",
+    "git_commit_hash": "30ea4c22082da628f44ddf690e253241babfb1c6",
+    "git_commit_date": "08.11.2024 12:16",
+    "app_version": "0.1.20",
 }
