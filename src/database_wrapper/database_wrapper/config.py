7110e84a412af82562bc60eb3fe72ba65b3ceba2
info@4apps.lv <Build Machine>
2024-11-08 19:06:53 +0000

    Updated project names in their respective README files

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "b8aa731944c60b0284b3f79220d373d228fdd95f",
-    "git_commit_date": "08.11.2024 18:42",
-    "app_version": "0.1.33",
+    "git_commit_hash": "dbaa41699186e5b63fb03eeed84fd11e0c0f83fb",
+    "git_commit_date": "08.11.2024 19:01",
+    "app_version": "0.1.34",
 }
