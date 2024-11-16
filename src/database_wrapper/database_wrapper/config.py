abe93a50096776c713406f23fe38fb1d008c89e4
Gints Murans <gm@gm.lv>
2024-11-16 02:18:41 +0000

    Added missing file

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "6408e9879dd432ba0f842413444ae68b9ff6e5a9",
-    "git_commit_date": "16.11.2024 02:15",
-    "app_version": "0.1.53",
+    "git_commit_hash": "4b4f3705d9cad627b12b1b6371734d942c03aacf",
+    "git_commit_date": "16.11.2024 02:18",
+    "app_version": "0.1.54",
 }
