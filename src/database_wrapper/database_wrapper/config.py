943e0234156c4a164061e5af9fb9dbd8c3041f6f
info@4apps.lv <Build Machine>
2024-11-26 22:09:20 +0000

    Fixed abc properties and added Composable to operation parameter

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "78c5e3f3d4453fdaeadf4a18cb317354b4088ee4",
-    "git_commit_date": "26.11.2024 21:17",
-    "app_version": "0.1.70",
+    "git_commit_hash": "bd0a207e8cc264ddf530cc191851c1ba4f981b35",
+    "git_commit_date": "26.11.2024 21:55",
+    "app_version": "0.1.71",
 }
