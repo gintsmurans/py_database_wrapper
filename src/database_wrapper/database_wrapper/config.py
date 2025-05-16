c8c3fb11449edca224b53b0f96dbfc6689c52813
info@4apps.lv <Build Machine>
2025-05-16 22:01:16 +0000

    Rename hasConnection and hasCursor methods to follow Python naming conventions

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "b117e5a27fe8408b8245baae74eda5aefdcce995",
-    "git_commit_date": "25.04.2025 21:32",
-    "app_version": "0.2.1",
+    "git_commit_hash": "c2bc2271cb75bcb46a0114013afa9b34dcb71aa6",
+    "git_commit_date": "16.05.2025 21:56",
+    "app_version": "0.2.2",
 }
