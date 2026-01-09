4baad61b81d9f178acd8de04c1a65f2053e06d89
info@4apps.lv <Build Machine>
2026-01-09 00:48:54 +0000

    Changed tools used for formatting and linting and more.

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "520e71902c6f09ba35b57fd541d2e82dc5c54478",
-    "git_commit_date": "06.01.2026 02:41",
-    "app_version": "0.2.14",
+    "git_commit_hash": "10407a63905bcc03c057145d5cf232fe84e18a9d",
+    "git_commit_date": "06.01.2026 13:14",
+    "app_version": "0.2.15",
 }
