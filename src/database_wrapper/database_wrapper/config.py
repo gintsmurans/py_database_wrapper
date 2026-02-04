523610ede14c2136f85bcb064d14972370deb889
Gints Murans <gm@gm.lv>
2026-02-04 14:10:55 +0200

    Bumped version to 0.2.24

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -3,7 +3,7 @@ from typing import Any
 CONFIG: dict[str, Any] = {
     # These are supposed to be set automatically by a git pre-compile script
     # They are one git commit hash behind, if used automatically
-    "git_commit_hash": "31a698fb162f0f3b837da41c631aadd26afa4973",
-    "git_commit_date": "10.01.2026 01:39",
-    "app_version": "0.2.23",
+    "git_commit_hash": "62c0c673862251219e0f5ea144907fbaaf35cba3",
+    "git_commit_date": "04.02.2026 14:08",
+    "app_version": "0.2.24",
 }
