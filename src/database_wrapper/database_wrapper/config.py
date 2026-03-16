63a40bacb8a5ffa356a08e6c763bc61d749fcb66
Gints Murans <gm@gm.lv>
2026-03-16 02:44:47 +0200

    Version bump

--- src/database_wrapper/database_wrapper/config.py
+++ src/database_wrapper/database_wrapper/config.py
@@ -5,5 +5,5 @@ CONFIG: dict[str, Any] = {
     # They are one git commit hash behind, if used automatically
     "git_commit_hash": "4f55ca3634ab996560f21fb8988b656f15947b78",
     "git_commit_date": "19.02.2026 02:01",
-    "app_version": "0.2.26",
+    "app_version": "0.2.27",
 }
